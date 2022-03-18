80e3d223ccde4ffb2b797879b08e020133b023bf
davronoff <ilyosbekdavronv009@gmail.com>
2022-03-18 16:22:40 +0500

    Add project files.

---
+++ MyBlog/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.Models;
+using System;
+using System.Collections.Generic;
+
+namespace MyBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Post> Posts;
+
+        public HomeController()
+        {
+            Posts = new List<Post>()
+            {
+                new Post()
+                {
+                    Id = 1,
+                    Title = ".NET bo'yicha maqola yozish mavsumimiz yana boshlandi  ",
+                    Body = "Agar kimdir kitob yozmoqchi bo'lsa Diyorbek Mamataliyev (http://t.me/diyorbek_mamataliyev) ga murojaat qilsin.",
+                    CreatedTime = DateTime.Now,
+                    ImagePath = "logo.jpg"
+                },
+                new Post()
+                {
+                    Id = 2,
+                    Title = ".NET bo'yicha maqola yozish mavsumimiz yana boshlandi  ",
+                    Body = "Agar kimdir kitob yozmoqchi bo'lsa Diyorbek Mamataliyev (http://t.me/diyorbek_mamataliyev) ga murojaat qilsin.",
+                    CreatedTime = DateTime.Now,
+                    ImagePath = "photo_2021-11-27_00-43-16.jpg"
+                },
+                new Post()
+                {
+                    Id = 3,
+                    Title = ".NET bo'yicha maqola yozish mavsumimiz yana boshlandi  ",
+                    Body = "Agar kimdir kitob yozmoqchi bo'lsa Diyorbek Mamataliyev (http://t.me/diyorbek_mamataliyev) ga murojaat qilsin.",
+                    CreatedTime = DateTime.Now,
+                    ImagePath = "photo_2021-11-27_00-43-32.jpg"
+                }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View(Posts);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyBlog/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
